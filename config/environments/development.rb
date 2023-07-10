5e7292acf03974c6ffdf93c147ef28eee55aa219
RyosukeSuzuki <138420782+Ryosuke2715@users.noreply.github.com>
2023-07-10 06:06:18 +0000

    [start] ２章学習開始

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  
+  config.hosts << "45fcb134747342edbd6cb3bceebe30aa.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
